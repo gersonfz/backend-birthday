4954d3b4f21d7c242a5e211c074c966d3e48aa79
Gerson Fernández <gersonfz17@gmail.com>
2023-07-19 05:30:58 -0300

    Change process.env.PORT

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
-import { ENV_CONFIG_PROCESS } from "./config.js";
 
-const PORT = ENV_CONFIG_PROCESS.PORT || 8080;
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port: ${PORT}`);
