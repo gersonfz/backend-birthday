cf3f316968f178678e1954cf11fa837fd541c5c3
Gerson Fernández <gersonfz17@gmail.com>
2023-07-19 19:32:14 -0300

    Configurando endpoints y probandolos en produccion

--- src/config.js
+++ src/config.js
@@ -3,5 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const ENV_CONFIG_PROCESS = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD
 };
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,11 +1,65 @@
 import { Router } from "express";
+import nodemailer from 'nodemailer';
+import { ENV_CONFIG_PROCESS } from "../config.js";
+
 
 const router = Router();
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: ENV_CONFIG_PROCESS.USER,
+        pass: ENV_CONFIG_PROCESS.PASSWORD
+    }
+});
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.json({ message: 'Correo enviado correctamente' });
+router.post('/form-birthday', async (req, res) => {
+        const { attendance, name, importantData } = req.body;
 
+        const contentHTML = `
+            <h1> ¿Asiste a la fiesta? </h1> 
+            <ul>
+                <li> Asiste: ${attendance} </li>
+                <li> Nombre: ${name} </li>
+                <li> Dato importante: ${importantData} </li>
+            </ul>
+        `
+    
+        const info = await transporter.sendMail({
+            from: "'XV - Milagros' <xvmilagros29@gmail.com>",
+            to:'gersonfz17@gmail.com',
+            subject: 'Asiste a la fiesta?',
+            html: contentHTML
+        });
+
+        console.log('Message sent: ', info.messageId);
+
+        res.json({ redirect: 'http://localhost:5173/' });
+    });
+
+router.post('/form-music', async (req, res) => {
+    const { name, song, link } = req.body;
+    const contentHTML = `
+        <h1> Musica </h1> 
+        <ul>
+            <li> Nombre: ${name} </li>
+            <li> Nombre de cancion o autor: ${song} </li>
+            <li> Enlace: ${link} </li>
+        </ul>
+    `
+
+
+    const info = await transporter.sendMail({
+        from: "'XV - Milagros' <xvmilagros29@gmail.com>",
+        to:'gersonfz17@gmail.com',
+        subject: 'Musica',
+        html: contentHTML
+    });
+
+    console.log('Message sent: ', info.messageId);
+
+    res.json({ redirect: 'http://localhost:5173/' });
 });
 
 export default router;
