9965e80cdc35adac45b9861dc00f3dce03cf920e
Gerson Fernández <gersonfz17@gmail.com>
2023-07-19 21:13:19 -0300

    Probando produccion con CORS

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-
+app.use(cors({
+    origin: '*'
+  }));
+  
 // Routes
 app.use('/send-mail', routerApi)
 app.use('/', (req, res) => res.send('<h1>Hola mundo</h1>'))
